6c7375944d571826e77819bae3482ddf86fb9e0f
LeQuadratico <45048954+LeQuadratico@users.noreply.github.com>
2021-01-23 13:11:03 +0100

    Add Day 18-1/18-2 (Part 2 not working correctly)

---
+++ Day 18-1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Day_18_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AdventOfCode - Day 18-1\n");
+
+            Console.WriteLine("Enter path to textfile:");
+            string path = Console.ReadLine();
+            Console.WriteLine();
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            long sum = 0;
+            foreach (string line in lines)
+            {
+                sum += Solve(line).Item1;
+            }
+
+            Console.WriteLine("The sum is " + sum);
+        }
+
+        static (long, int) Solve(string s)
+        {
+            long value = 0;
+            bool addOperator = true;
+
+            int pointer = 0;
+
+            while (pointer < s.Length)
+            {
+                int number = 0;
+                int pos = 0;
+                if (int.TryParse(s[pointer].ToString(), out number))
+                {
+                    if (addOperator)
+                        value += number;
+                    else
+                        value *= number;
+                }
+                else if (s[pointer] == '(')
+                {
+                    (long result, int pos) solveResult = Solve(s.Substring(pointer + 1));
+
+                    if (addOperator)
+                        value += solveResult.result;
+                    else
+                        value *= solveResult.result;
+
+                    pos = solveResult.pos;
+                }
+                else if (s[pointer] == ')')
+                {
+                    return (value, pointer + 2);
+                }
+                else if (s[pointer] == '*')
+                {
+                    addOperator = false;
+                }
+                else if (s[pointer] == '+')
+                {
+                    addOperator = true;
+                }
+
+                if (pos != 0)
+                {
+                    pointer += pos;
+                    pos = 0;
+                }
+                else
+                    pointer++;
+            }
+
+            return (value, 0);
+        }
+    }
+}
---
+++ Day 18-2/Program.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_18_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AdventOfCode - Day 18-2\n");
+
+            Console.WriteLine("Enter path to textfile:");
+            string path = Console.ReadLine();
+            Console.WriteLine();
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+
+            ulong sum = 0;
+            foreach (string line in lines)
+            {
+                List<Number> numbers = new List<Number>();
+                Dictionary<int, Number> numberAtDepth = new Dictionary<int, Number>();
+                int pointer = 0;
+                int depth = 0;
+
+                numberAtDepth.Add(depth, new Number());
+                numbers.Add(numberAtDepth[depth]);
+
+                while (pointer < line.Length)
+                {
+                    if (line[pointer] == '(')
+                    {
+                        numberAtDepth.Add(depth + 1, new Number(numberAtDepth[depth], depth + 1));
+                        depth++;
+                    }
+                    else if (line[pointer] == ')')
+                    {
+                        numberAtDepth[depth].s += line[pointer];
+                        numbers.Add(numberAtDepth[depth]);
+                        numberAtDepth.Remove(depth);
+                        depth--;
+                    }
+
+                    for (int i = 0; i < depth + 1; i++)
+                    {
+                        numberAtDepth[depth - i].s += line[pointer];
+                    }
+
+                    pointer++;
+                }
+
+
+
+                int deepest = 0;
+
+                foreach (Number n in numbers)
+                    if (n.depth > deepest)
+                        deepest = n.depth;
+
+                while (true)
+                {
+                    foreach (Number n in numbers)
+                    {
+                        if (n.depth != deepest)
+                            continue;
+
+                        n.SolveAndCorrectParent();
+                    }
+                    deepest--;
+                    if (deepest == -1)
+                        break;
+                }
+
+                sum += (ulong)numberAtDepth[0].value;
+
+                //Console.WriteLine(numberAtDepth[0].value);
+
+            }
+
+            Console.WriteLine("The sum is " + sum);
+        }
+
+        class Number
+        {
+            public string s = string.Empty;
+            public string startString;
+            public int depth;
+            public Number parent;
+            public long value;
+
+            public Number(Number parent, int depth)
+            {
+                this.depth = depth;
+                this.parent = parent;
+            }
+
+            public Number()
+            {
+                depth = 0;
+            }
+
+            public void SolveAndCorrectParent()
+            {
+                string debugStart = s;
+
+                if (long.TryParse(s, out _))
+                    return;
+
+                if (startString == null)
+                    startString = s;
+
+                bool addOperator = false;
+                int pointer = 0;
+                long lastNumber = -1;
+                string numberstring = string.Empty;
+                //Additions
+                while (pointer < s.Length)
+                {
+                    long number = 0;
+
+                    if (long.TryParse(s[pointer].ToString(), out number))
+                    {
+                        numberstring += s[pointer];
+                    }
+                    else if (s[pointer] == '*')
+                    {
+                        addOperator = false;
+                    }
+                    else if (s[pointer] == '+')
+                    {
+                        addOperator = true;
+                    }
+                    else if (s[pointer] == ' ')
+                    {
+                        if (numberstring != string.Empty)
+                        {
+                            number = long.Parse(numberstring);
+                            numberstring = string.Empty;
+
+                            if (addOperator && lastNumber != -1)
+                            {
+                                s = s.Replace((lastNumber + " + " + number), (lastNumber + number).ToString());
+                                pointer = 0;
+                                lastNumber = -1;
+                                addOperator = false;
+                                continue;
+                            }
+                            else
+                                lastNumber = number;
+                        }
+                    }
+
+                    pointer++;
+                }
+                if (numberstring != string.Empty)
+                {
+                    long number = long.Parse(numberstring);
+                    numberstring = string.Empty;
+
+                    if (addOperator && lastNumber != -1)
+                    {
+                        s = s.Replace((lastNumber + " + " + number), (lastNumber + number).ToString());
+                        pointer = 0;
+                        lastNumber = -1;
+                        addOperator = false;
+                    }
+                    else
+                        lastNumber = number;
+                }
+
+                //Multiplications
+                pointer = 0;
+                addOperator = true;
+                lastNumber = -1;
+                numberstring = string.Empty;
+                while (pointer < s.Length)
+                {
+                    long number = 0;
+                    if (long.TryParse(s[pointer].ToString(), out number))
+                    {
+                        numberstring += s[pointer];
+                    }
+                    else if (s[pointer] == '*')
+                    {
+                        addOperator = false;
+                    }
+                    else if (s[pointer] == '+')
+                    {
+                        addOperator = true;
+                    }
+                    else if (s[pointer] == ' ')
+                    {
+                        if (numberstring != string.Empty)
+                        {
+                            number = long.Parse(numberstring);
+                            numberstring = string.Empty;
+
+                            if (!addOperator && lastNumber != -1)
+                            {
+                                s = s.Replace((lastNumber + " * " + number), (lastNumber * number).ToString());
+                                lastNumber = -1;
+                                pointer = 0;
+                                addOperator = true;
+                                continue;
+                            }
+                            else
+                                lastNumber = number;
+                        }
+                    }
+
+                    pointer++;
+                }
+                if (numberstring != string.Empty)
+                {
+                    long number = long.Parse(numberstring);
+                    numberstring = string.Empty;
+
+                    if (!addOperator && lastNumber != -1)
+                    {
+                        s = s.Replace((lastNumber + " * " + number), (lastNumber * number).ToString());
+                        lastNumber = -1;
+                        pointer = 0;
+                        addOperator = true;
+                    }
+                    else
+                        lastNumber = number;
+                }
+
+                if (parent != null)
+                {
+                    s = s.Substring(1);
+                    s = s.Remove(s.Length - 1);
+                }
+
+                if (parent != null)
+                    parent.CorrentPart(startString, long.Parse(s));
+                else
+                    value = long.Parse(s);
+
+            }
+
+            public void CorrentPart(string s, long result)
+            {
+                if (startString == null)
+                    startString = this.s;
+
+                this.s = this.s.Replace(s, result.ToString());
+            }
+        }
+    }
+}
